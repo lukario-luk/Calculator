464943239b7485ad1d6c660e0b2965e45c7ea13b
Lucas-de-Souza-Pinto <lucasdesouzapinto2000@gmail.com>
2022-03-04 17:35:11 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Calc extends Component{
+  state={
+    n1: '',
+    n2: '',
+    result: 0,
+  }
+  handleChange = (event) => {
+    this.setState({
+      n1: Number(event.target.value)
+    })
+  }
+  handleChange2 = (event) => {
+    this.setState({
+      n2: Number(event.target.value)
+    })
+  }
+mult = () => {
+  const {n1, n2} = this.state
+  if (n1 !== ''){
+this.setState({
+  result: n1 * n2
+})
+  }
+}
+div= () => {
+  const {n1, n2} = this.state
+  if (n1 && n2 !== ''){
+    this.setState({
+      result: n1 / n2
+    })
+      }
+}
+subt= () => {
+  const {n1, n2} = this.state
+  if (n1 && n2 !== ''){
+    this.setState({
+      result: n1 - n2
+    })
+      }
+}
+some= () => {
+  const {n1, n2} = this.state
+  if (n1 && n2 !== ''){
+    this.setState({
+      result: n1 + n2
+    })
+      }
+}
+clearCalc = () => {
+  this.setState({
+    result:"",
+    n1:"",
+    n2:"",
+  })
 }
 
-export default App;
+render(){
+  return(
+    <div>
+      <h1> react app calc</h1>
+      <input value={this.state.n1} onChange={this.handleChange} type="number"/>
+      <input value={this.state.n2} onChange={this.handleChange2} type="number"/>
+      <button onClick={this.mult}>*</button>
+      <button onClick={this.div}>/</button>
+      <button onClick={this.subt}>-</button>
+      <button onClick={this.some}>+</button>
+      <button onClick={this.clearCalc} >⌫</button>
+      <h2>{this.state.result}</h2>
+    </div>
+  )
+}
+
+}
+
+export default Calc;
